3756cd6e0f99c3b1b92db67e7fbb9e6bd98a6cce
Merlintor <merlinfuchs2001@gmail.com>
2020-05-10 17:09:20 +0200

    Apply camera rotation to servos

--- modules/camera.py
+++ modules/camera.py
@@ -2,11 +2,20 @@ from module import Module, listener, http_route
 from aiohttp import web, MultipartWriter
 import asyncio
 import time
+from enum import IntEnum
 from uuid import uuid4
 import cv2
+import RPi.GPIO as GPIO
 
 
+GPIO.setmode(GPIO.BOARD)
 USB_ID = 0
+MAX_ANGLE = 140
+
+
+class GPIOPins(IntEnum):
+    SERVO_X = 16
+    SERVO_Y = 18
 
 
 class CameraModule(Module):
@@ -22,24 +31,44 @@ class CameraModule(Module):
         }
 
         self._goal_fps = 30
+        self.loop.run_in_executor(None, self._setup_gpio)
 
-    async def get_cam_state(self):
-        """
-        Get current x- and y-rot of the connected cam
-        """
-        pass
+    def _setup_gpio(self):
+        GPIO.setup(GPIOPins.SERVO_X, GPIO.OUT)
+        GPIO.setup(GPIOPins.SERVO_Y, GPIO.OUT)
+        GPIO.output(GPIOPins.SERVO_X, GPIO.LOW)
+        GPIO.output(GPIOPins.SERVO_Y, GPIO.LOW)
+
+        self.set_rotation(GPIOPins.SERVO_X, 0)
+        self.set_rotation(GPIOPins.SERVO_Y, 0)
+
+    def set_rotation(self, pin, rotation):
+        min_width = 0.000750
+        max_width = 0.002250
+
+        pulse_width = ((rotation / MAX_ANGLE) * (max_width - min_width)) + min_width
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(pulse_width)
+        GPIO.output(pin, GPIO.LOW)
 
     async def move_cam(self):
         """
         Apply x- and y-rot to the connected cam
         """
-        pass
+        xrot = self.state["xrot"] + 30
+        yrot = self.state["yrot"] + 70
+        tasks = (
+            self.loop.run_in_executor(None, self.set_rotation, GPIOPins.SERVO_X, xrot),
+            self.loop.run_in_executor(None, self.set_rotation, GPIOPins.SERVO_Y, yrot),
+        )
+        return await asyncio.wait(tasks)
 
     @listener("cam_move")
     async def on_cam_move(self, ws, payload):
         try:
-            new_xrot = int(payload["xrot"])
-            new_yrot = int(payload["yrot"])
+            # Clip angle to 0 - 140
+            new_xrot = min(max(-30, int(payload["xrot"])), 110)
+            new_yrot = min(max(-70, int(payload["yrot"])), 70)
 
             await self.state_update({
                 "xrot": new_xrot,
@@ -71,7 +100,7 @@ class CameraModule(Module):
             if not rc:
                 continue
 
-            result, data = cv2.imencode(".jpg", frame, (int(cv2.IMWRITE_JPEG_QUALITY), 50))
+            result, data = cv2.imencode(".jpg", frame, (int(cv2.IMWRITE_JPEG_QUALITY), 100))
 
             with MultipartWriter('image/jpeg', boundary=boundary) as mpwriter:
                 mpwriter.append(data.tostring(), {'Content-Type': 'image/jpeg'})
